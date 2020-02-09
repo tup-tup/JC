8cf22b2870720c0de7670ba7757ae12fd838faf5
tup-tup <60755294+tup-tup@users.noreply.github.com>
2020-02-09 23:07:32 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,29 +7,6 @@ var mySwiper = new Swiper('.swiper-container', {
     },
 });
 
-// var menuButton = document.querySelector('.burger__btn');
-// var menu = document.querySelector('.burger__menu'); 
-//     menuButton.addEventListener ('click', function() {
-//     ValidityState.
-//     menuButton.classList.toggle('burger__btn')
-//     menu.classList.toggle('burger__menu_active');
-// });
-
-// $('.burger__btn').on('click',function(){
-//     $('.burger__menu_active').toggle(300);
-// })
-
-// $('#closeMenu').on('click',function(){
-//     $('.head__menu').hide(300);
-// })
-
-// document.getElementsByClassName(".burger__btn").onclick = function() {
-//     open()
-// };
-
-// function open() {
-//     document.getElementsByClassName(".burger__menu").classList.toggle("active")
-// 
 
 $(function(){
     $(".burger__btn").on('click', function() {
