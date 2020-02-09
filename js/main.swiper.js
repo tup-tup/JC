9a8a2d40c80e4005a9a845b9c079b2f621e9a36f
tup-tup <60755294+tup-tup@users.noreply.github.com>
2020-02-09 23:40:48 +0300

    Add files via upload

---
+++ js/main.swiper.js
@@ -0,0 +1,20 @@
+// var mySwiper = new Swiper('.swiper-container', {
+//     slidesPerView : 3,
+//     loop: true,
+//     navigation: {
+//         nextEl: '.news__btn_next',
+//         prevEl: '.news__btn_prev',
+//     },
+// });
+
+var mySwiper = new Swiper ('.swiper-container', {
+    direction : 'horizontal',
+    spaceBetween : 50,
+    slidesPerView: 3,
+    loop : true,
+    navigation: {
+        nextEl: '.news__btn_next',
+        prevEl: '.news__btn_prev',
+    },
+});
+
